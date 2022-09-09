ef8d29ec0f72223f9e97ab61a8e4ab916e6a7ade
Alexander Nguyen <cernworks@gmail.com>
2022-09-09 04:21:39 -0400

    tabs for sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import Login from './components/Login';
 import Dashboard from './components/Dashboard'
 import React from 'react'
 import useLocalStorage from './hooks/useLocalStorage';
- 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   const [id, setId] = useLocalStorage()
 
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tab, Nav } from 'react-bootstrap'
+
+import Conversations from './Conversations'
+import Contacts from './Contacts'
 
 const CONVERSATIONS = 'conversations'
 const CONTACTS = 'contacts'
 
-export default function Sidebar( {id} ) {
-  return (
-    <div style={{ width: '250px'} } className="d-flex flex-column">
-        <Tab.Container>
-            <Nav>
-                <Nav.Item>
-                    <Nav.Link eventKey="conversations"> Conversations </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKeys="contacts"> Contacts </Nav.Link>
-                </Nav.Item>
-            </Nav>
-        </Tab.Container>
-    </div>
-  )
+export default function Sidebar({ id }) {
+    const [activeKey, setActiveKey] = useState(CONVERSATIONS)
+
+    return (
+        <div style={{ width: '250px' }} className="d-flex flex-column">
+            <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
+                <Nav variant="tabs" className="justify-content-center">
+                    <Nav.Item>
+                        <Nav.Link eventKey={CONVERSATIONS}> Conversations </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey={CONTACTS}> Contacts </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <Tab.Content className="border-end 1px overflow-auto flex-grow-1">
+                    <Tab.Pane eventKey={CONVERSATIONS}>
+                        <Conversations />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey={CONTACTS}>
+                        <Contacts />
+                    </Tab.Pane>
+                </Tab.Content>
+            </Tab.Container>
+        </div>
+    )
 }
