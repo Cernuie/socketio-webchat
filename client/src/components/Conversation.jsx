55bbb8ffd9abf711e3b947767f8fafb2fb93f8a1
Alexander Nguyen <cernworks@gmail.com>
2022-09-14 13:09:20 -0400

    optional chaining for conversations storage

--- client/src/components/Conversation.jsx
+++ client/src/components/Conversation.jsx
@@ -25,8 +25,8 @@ export default function Conversation() {
         <div className='d-flex flex-column flex-grow-1'>
             <div className='flex-grow-1 overflow-auto'>
                 <div className='d-flex flex-column align-items-start justify-content-end px-3'>
-                    {selectedConversation.messages.map((message, index) => {
-                        const lastMessage = selectedConversation.messages.length - 1 === index
+                    {selectedConversation?.messages.map((message, index) => {
+                        const lastMessage = selectedConversation?.messages.length - 1 === index
                         return (
                             <div 
                             ref={lastMessage ? setRef : null}
--- client/src/contexts/ConversationsProvider.jsx
+++ client/src/contexts/ConversationsProvider.jsx
@@ -21,25 +21,6 @@ export function ConversationsProvider({ id, children }) {
         })
     }
 
-    const formattedConversations = conversations.map((conversation, index) => {
-        const recipients = conversation.recipients.map(recipient => {
-            const contact = contacts.find(contact => {
-                return contact.id === recipient
-            })
-            const name = (contact && contact.name) || recipient
-            return { id: recipient, name }
-        })
-        const messages = conversation.messages.map(message => {
-            const contact = contacts.find(contact => {
-                return contact.id === message.sender
-            })
-            const name = (contact && contact.name) || message.sender
-            const fromMe = id === message.sender
-            return { ...message, senderName: name, fromMe }
-        })
-        const selected = index === selectedConversationIndex
-        return { ...conversation, recipients, selected, messages }
-    })
 
     const addMessageToConversation = useCallback(({ recipients, text, sender }) => {
         setConversations(prevConversations => {
@@ -72,11 +53,32 @@ export function ConversationsProvider({ id, children }) {
         }
     }, [socket, addMessageToConversation])
 
-
     function sendMessage(recipients, text) {
         socket.emit('send-message', { recipients, text })
         addMessageToConversation({ recipients, text, sender: id })
     }
+
+    const formattedConversations = conversations.map((conversation, index) => {
+        const recipients = conversation.recipients.map(recipient => {
+            const contact = contacts.find(contact => {
+                return contact.id === recipient
+            })
+            const name = (contact && contact.name) || recipient
+            return { id: recipient, name }
+        })
+
+        const messages = conversation.messages.map(message => {
+            const contact = contacts.find(contact => {
+                return contact.id === message.sender
+            })
+            const name = (contact && contact.name) || message.sender
+            const fromMe = id === message.sender
+            return { ...message, senderName: name, fromMe }
+        })
+
+        const selected = index === selectedConversationIndex
+        return { ...conversation, messages, recipients, selected }
+    })
 
 
     const value = {
