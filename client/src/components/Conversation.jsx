37bbbc6bb7fa613bdd87013341c3cb0de875f63d
Alexander Nguyen <cernworks@gmail.com>
2022-09-14 12:29:13 -0400

    messages scroll properly

--- client/src/components/Conversation.jsx
+++ client/src/components/Conversation.jsx
@@ -4,6 +4,11 @@ import { useConversations } from '../contexts/ConversationsProvider'
 
 export default function Conversation() {
     const [text, setText] = useState('')
+    const setRef = useCallback(node => {
+        if (node){
+            node.scrollIntoView({ smooth: true })            
+        }
+    }, [])
     const { sendMessage, selectedConversation } = useConversations()
 
     function handleSubmit(e) {
@@ -19,10 +24,12 @@ export default function Conversation() {
     return (
         <div className='d-flex flex-column flex-grow-1'>
             <div className='flex-grow-1 overflow-auto'>
-                <div className='h-100 d-flex flex-column align-items-start justify-content-end px-3'>
+                <div className='d-flex flex-column align-items-start justify-content-end px-3'>
                     {selectedConversation.messages.map((message, index) => {
+                        const lastMessage = selectedConversation.messages.length - 1 === index
                         return (
                             <div 
+                            ref={lastMessage ? setRef : null}
                             key={index} 
                             className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end' : ''}`}>
                                 <div 
