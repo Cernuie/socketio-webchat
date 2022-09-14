3094e276679249db09c03346e3c117f62521bfd4
Alexander Nguyen <cernworks@gmail.com>
2022-09-14 12:18:41 -0400

    messages showing

--- client/src/components/Conversation.jsx
+++ client/src/components/Conversation.jsx
@@ -19,7 +19,24 @@ export default function Conversation() {
     return (
         <div className='d-flex flex-column flex-grow-1'>
             <div className='flex-grow-1 overflow-auto'>
-
+                <div className='h-100 d-flex flex-column align-items-start justify-content-end px-3'>
+                    {selectedConversation.messages.map((message, index) => {
+                        return (
+                            <div 
+                            key={index} 
+                            className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end' : ''}`}>
+                                <div 
+                                    className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
+                                    {message.text}
+                                </div>
+                                <div 
+                                className={`text-muted small ${message.fromMe ? 'text-right' : ''}`}>
+                                    {message.fromMe ? 'You' : message.senderName}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className='m-2'>
