d9d8326ff90f524349ceda5fda3b3024fb995fe4
Alexander Nguyen <cernworks@gmail.com>
2022-09-14 00:05:42 -0400

    selected conversation functionality

--- client/src/components/Conversations.jsx
+++ client/src/components/Conversations.jsx
@@ -3,13 +3,15 @@ import { ListGroup } from 'react-bootstrap'
 import { useConversations } from '../contexts/ConversationsProvider'
 
 export default function Conversations() {
-    const { conversations } = useConversations()
+    const { conversations, selectConversationIndex } = useConversations()
 
   return (
     <ListGroup variant="flush">
       {conversations.map((conversation, index) => (
         <ListGroup.Item
           key={index}
+          action
+          onClick = {() => selectConversationIndex(index)}
           active={conversation.selected}
         >
           {conversation.recipients.map(r => r.name).join(', ')}
--- client/src/contexts/ConversationsProvider.jsx
+++ client/src/contexts/ConversationsProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import useLocalStorage from '../hooks/useLocalStorage'
 import { useContacts } from './ContactsProvider'
 
@@ -10,6 +10,7 @@ export function useConversations() {
   
 export function ConversationsProvider({ children }) {
     const [conversations, setConversations] = useLocalStorage('conversations', [])
+    const [selectedConversationIndex, setSelectedConversationIndex] = useState(0)
     const { contacts } = useContacts()
 
     function createConversation(recipients) {
@@ -18,23 +19,28 @@ export function ConversationsProvider({ children }) {
         })
       }
 
-    const formattedConversations = conversations.map(conversation => {
+    const formattedConversations = conversations.map((conversation, index) => {
         const recipients = conversation.recipients.map(recipient => {
             const contact = contacts.find(contact => {
                 return contact.id === recipient
             })
             const name = (contact && contact.name) || recipient
             return { id: recipient, name }
-        })
+        })  
+        const selected = index === selectedConversationIndex
+        return { ...conversation, recipients, selected }
     })
+    
 
     const value = {
         conversations: formattedConversations,
+        selectedConversationIndex: formattedConversations[selectedConversationIndex],
+        selectConversationIndex: setSelectedConversationIndex,
         createConversation
     }
     
     return (
-        <ConversationsContext.Provider value={{ value }}>
+        <ConversationsContext.Provider value={ value }>
             {children}
         </ConversationsContext.Provider>
     )
